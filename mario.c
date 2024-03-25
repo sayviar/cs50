81df8deef53c5299c4e4c086ae92c0827acb365f
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-25 09:46:31 +0000

    Mon, Mar 25, 2024, 10:46 AM +01:00

--- mario.c
+++ mario.c
@@ -3,11 +3,12 @@
 
 int main(void)
 {
-    int n = get_int("Size: ");
-    while ( n < 1)
+    int n
+    do
     {
         n = get_int("Size: ");
     }
+    while (n < 1);
     for (int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
