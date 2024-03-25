e07cef6781b5448f77382444517f894467cfb754
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-25 09:53:06 +0000

    Mon, Mar 25, 2024, 10:53 AM +01:00

--- mario.c
+++ mario.c
@@ -3,12 +3,15 @@
 
 int main(void)
 {
+    // Prompt user for positive integer
     int n;
     do
     {
         n = get_int("Size: ");
     }
     while (n < 1);
+
+    // Print an n-by-n grid of bricks
     for (int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
