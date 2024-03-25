a56d913dfc6de16323ab9cf9cfc7d7048ffb3b6c
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-25 10:18:10 +0000

    Mon, Mar 25, 2024, 11:18 AM +01:00

--- calculator.c
+++ calculator.c
@@ -9,13 +9,14 @@ int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
+    float z = (float) x / y;
 
     for(char i[20]= ""; strlen(i) < 10; strcat(i, "r"))
     {
         printf("%s\n", i);
     }
 
-    printf("%i\n", add(x, y));
+    printf("%f\n", z);
 }
 
 
