3035b26c06ff0b1e434b2e207429c1d26b0f02ab
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 12:41:56 +0000

    Tue, Mar 26, 2024, 1:41 PM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int minimumCoins(int change);
+int calculate25(int change);
+int calculate10(int change);
+int calculate5(int change);
+int calculate1(int change);
 int main(void)
 {
     int change = get_int("Change owed: ");
+    int minimumCoins = 0;
+    minimumCoins += calculate25(change);
+    minimumCoins += calculate10(change);
+    minimumCoins += calculate5(change);
+    minimumCoins += calculate1(change);
+    printf("%i\n", minimumCoins);
+
 
 }
 
 int calculate25(int change)
 {
-    int coins;
+    int coins = 0;
     while (change > 25)
     {
         coins++;
@@ -21,21 +31,29 @@ int calculate25(int change)
 
 int calculate10(int change)
 {
-    int coins;
+    int coins = 0;
     while(change > 10)
     {
         coins++;
         change -= 10;
     }
+    return coins;
 }
 
 int calculate5(int change)
 {
-    int coins;
+    int coins = 0;
     while(change > 5)
     {
         coins++;
         change -= 5;
     }
+    return coins;
 }
+
+int calculate1(int change)
+{
+    return change;
+}
+
 
