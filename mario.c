d925779c8a6bb7f303ebadb7c2411cd07f613485
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 07:47:06 +0000

    Tue, Mar 26, 2024, 8:47 AM +01:00

--- mario.c
+++ mario.c
@@ -1,24 +1,9 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    // Prompt user for positive integer
-    int n;
-    do
-    {
-        n = get_int("Size: ");
-    }
-    while (n < 1);
+    int n = get_int("How high should the pyramid be?\n");
 
-    // Print an n-by-n grid of bricks
-    for (int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
-
+    for (int i = 0; i)
 }
