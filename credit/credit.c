d662111aea03403c4d1236d63a8e5bf697f8f488
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 09:52:20 +0000

    Tue, Mar 26, 2024, 10:52 AM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,6 +8,11 @@ int main(void)
     {
         cardnumber = get_long("Number: ");
     }
-    while (False)
-    for ()
+    while (False);
+
+    for (int i = 0; i % 2 == 1; i++)
+        for (int number = cardnumber; number > 0; number / 10)
+        {
+
+        }
 }
