48b88ab96df5c0858bb716e68d50f1cbafab0954
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 07:51:32 +0000

    Tue, Mar 26, 2024, 8:51 AM +01:00

--- mario.c
+++ mario.c
@@ -3,7 +3,10 @@
 
 int main(void)
 {
+    do {
     int n = get_int("How high should the pyramid be?\n");
+    }
+    while (n > 0 && n < 9);
 
-    for (int i = 0; i)
+    for (int i = 0; i < n;)
 }
