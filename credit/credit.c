d3478088f9c9570543aa3a4dcd76dea75cf664f8
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 10:09:56 +0000

    Tue, Mar 26, 2024, 11:09 AM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,7 +11,7 @@ int main(void)
     }
     while (false);
 
-    for (int number = cardnumber / 10; number > 0; number / 100)
+    for (long number = cardnumber / 10; number > 0; number / 100)
     {
         if ((number % 10) *2 > 9)
         {
@@ -23,7 +23,7 @@ int main(void)
             checksum += (number % 10) *2;
         }
     }
-    for (int n = cardnumber; n > 0; n / 100)
+    for (long n = cardnumber; n > 0; n / 100)
     {
         checksum += n % 10;
     }
