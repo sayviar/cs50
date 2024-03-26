8db407d7c4c8f70fd29c80b5be939b66a1ee065b
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 07:38:36 +0000

    Tue, Mar 26, 2024, 8:38 AM +01:00

--- hello2.c
+++ hello2.c
@@ -3,6 +3,9 @@
 
 int main(void)
 {
-    string user = get_string("What's your name?\n");
-    printf("Hello, %s!\n", user);
+    string first_name = get_string("What's your first name?\n");
+    string last_name = get_string("What's your last name?\n");
+    int age = get_int("What's your age?\n");
+    string phonenumber = get_string("What's your phone number?\n ");
+    printf("Hello, %s %s your age is %i and your phone number is %s!\n", first_name, last_name, age, phonenumber);
 }
