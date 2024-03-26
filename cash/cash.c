60bb735d0bf29cd4012c99a0743828f2a7db13d9
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 12:27:51 +0000

    Tue, Mar 26, 2024, 1:27 PM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int minimumCoins(int change);
 int main(void)
 {
+    int change = get_int("Change owed: ");
 
 }
 
-int change()
+int minimumCoins(int change)
+{
+    int coins = 0;
+    coins += change / 25;
+    change 
+}
