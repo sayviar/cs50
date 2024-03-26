bd98cde0e79fba1fe9f0c1dd831dcb3378bc671c
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 10:46:43 +0000

    Tue, Mar 26, 2024, 11:46 AM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 void ValidCheck(string card, long cardnumber);
 
@@ -8,15 +9,15 @@ int main(void)
 
     long cardnumber = get_long("Number: ");
     string card;
-    if (cardnumber / 10 * 14 == 34 || cardnumber / 10 * 14 == 37)
+    if (cardnumber / pow(10, 14) == 34 || cardnumber / pow(10, 14) == 37)
     {
         ValidCheck("AMEX\n", cardnumber);
     }
-    else if (cardnumber / 10  15 > 50 && cardnumber / 10 * 15 < 56)
+    else if (cardnumber / pow(10, 15) > 50 && cardnumber / pow(10, 15) < 56)
     {
         ValidCheck("MASTERCARD\n", cardnumber);
     }
-    else if (cardnumber / 10 * 13 == 4 || cardnumber / 10 * 16 == 4)
+    else if (cardnumber / pow(10, 13) == 4 || cardnumber / pow(10, 16) == 4)
     {
         ValidCheck("VISA\n", cardnumber);
     }
