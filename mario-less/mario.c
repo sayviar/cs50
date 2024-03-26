61f2fc16ee82eac05e0951a3025b4e226b39e938
sayviar <136386001+sayviar@users.noreply.github.com>
2024-03-26 09:06:21 +0000

    Tue, Mar 26, 2024, 10:06 AM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,10 +9,18 @@ int main(void)
     {
         height = get_int("Height: ");
     }
-    while (height < 1)
+    while (height < 1);
 
     for (int i = 0; i < height ; i++)
     {
-        for(int j = 0; j>)
+        for(int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+        for(int k = i; k > 0; k--)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }
