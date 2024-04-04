3ceb29b900d1c7352074fd56e76a8f4eb0514335
sayviar <136386001+sayviar@users.noreply.github.com>
2024-04-04 14:22:54 +0000

    Thu, Apr 4, 2024, 4:22 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -72,7 +72,7 @@ int SentenceCount(string text)
     int sentences = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (text[i] )
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?' )
         {
             sentences++;
         }
