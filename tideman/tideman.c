4e8e9aa673dba66653425db43be6d060904c9552
sayviar <136386001+sayviar@users.noreply.github.com>
2024-04-16 09:53:17 +0000

    Tue, Apr 16, 2024, 11:53 AM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -205,7 +205,14 @@ void print_winner(void)
     for (int i = 0; i < candidate_count; i++)
     {
         bool neverlost = true;
-        for
+        for (int j = 0; j < candidate_count; i++)
+        {
+            if (locked[i][j] == true)
+            {
+                neverlost = false;
+            }
+
+        }
         locked
     }
     return;
