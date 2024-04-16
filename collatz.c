72ff1f71d2f9469d44c9787e048084e6df2d9e0a
sayviar <136386001+sayviar@users.noreply.github.com>
2024-04-16 12:37:31 +0000

    Tue, Apr 16, 2024, 2:37 PM +02:00

--- collatz.c
+++ collatz.c
@@ -2,29 +2,26 @@
 #include <stdio.h>
 
 int collatz_count = 0;
-void collatz(int n);
+int collatz(int n);
 int main (void)
 {
-    collatz(get_int("Type int:"));
-    printf("%i\n", collatz_count);
+
+    printf("%i\n", collatz(get_int("Type int:")));
     return 0;
 }
 
-void collatz(int n)
+int collatz(int n)
 {
     if (n == 1)
     {
-        return;
+        return 0;
     }
     else if (n % 2 == 0)
     {
-        collatz(n/2);
-        collatz_count++;
-
+        return 1 + collatz(n/2);
     }
     else
     {
-        collatz(3* n +1);
-        collatz_count++;
+        return 1 + collatz(3* n +1);
     }
 }
