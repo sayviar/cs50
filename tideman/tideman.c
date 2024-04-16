302be70e04a1b63b167cf3628e83de8aa3b95df8
sayviar <136386001+sayviar@users.noreply.github.com>
2024-04-16 08:27:23 +0000

    Tue, Apr 16, 2024, 10:27 AM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -143,7 +143,13 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    for 
+    for (int i = 0; i < pair_count; i++)
+    {
+        for (int j = i + 1; j < pair_count; j++)
+        {
+            if preferences[pairs[j].winner][]
+        }
+    }
     return;
 }
 
