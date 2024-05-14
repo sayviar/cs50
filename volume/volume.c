a792edee6781e8aa91172d28b07e7bd9a3b2f38c
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-14 09:29:59 +0000

    Tue, May 14, 2024, 11:29 AM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,9 +43,9 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    fread(buffer, sizeof(int16_t), 1, input);
-    buffer = buffer * factor;
-    fwrite(buffer, sizeof(int16_t), 1, output);
+    fread(&buffer, sizeof(int16_t), 1, input);
+    buffer *=  factor;
+    fwrite(&buffer, sizeof(int16_t), 1, output);
 
     // Close files
     fclose(input);
