eaf9a632ec77d0715b799f52aeff310f64e42727
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-14 08:57:24 +0000

    Tue, May 14, 2024, 10:57 AM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,8 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-
+    FILE *output = fopen("output.wav", "w")
+    fread
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
