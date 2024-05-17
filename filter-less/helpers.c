4a6a3c4c74382918ef71486a7f72134ad00f221e
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-17 11:27:36 +0000

    Fri, May 17, 2024, 1:27 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -51,7 +51,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width/2.0; j++)
         {
-            
+            int buffer[] = image[i][j];
+            for (int k = 1; k< 3 ; k++)
+            {
+            printf("%i",buffer[k]);
+            }
         }
     }
     return;
