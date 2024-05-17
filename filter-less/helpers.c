bd5644b8ca3b543bfb5ada14208b77b418280cc0
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-17 11:58:18 +0000

    Fri, May 17, 2024, 1:58 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -53,8 +53,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width/2.0; j++)
         {
             RGBTRIPLE buffer = image[i][j];
-            image[i][j] = image[height-i][width-j];
-            image[height-i][width-j] = buffer;
+            image[i][j] = image[i][width-j];
+            image[i][width-j] = buffer;
         }
     }
     return;
