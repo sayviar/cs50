7efca8fdaa69483e9e15d93af97b357b9efce70f
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-17 10:11:41 +0000

    Fri, May 17, 2024, 12:11 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -32,9 +32,11 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaBlue;
 
             sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
-            if (sepiaRed > 255)
+            (sepiaRed > 255) ? (image[i][j].rgbtRed = 255) : (image[i][j].rgbtRed = sepiaRed);
             sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            (sepiaGreen > 255) ? (image[i][j].rgbtGreen = 255) : (image[i][j].rgbtGreen = sepiaGreen);
             sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+            (sepiaBlue > 255) ? (image[i][j].rgbtBlue = 255) : (image[i][j].rgbtBlue = sepiaBlue);
         }
     }
 
