195d1baae39dd3497f19f396c685e30dd075420f
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-17 09:54:27 +0000

    Fri, May 17, 2024, 11:54 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,8 +8,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int average 
-            image[i][j].rgbtRed
+            int average;
+            average = ((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0)
         }
     }
         //take average of red, green, and blue
