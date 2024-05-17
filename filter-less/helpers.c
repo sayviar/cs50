334e318f57b78a10dc143ac44ca37582cfdca7cc
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-17 13:11:07 +0000

    Fri, May 17, 2024, 3:11 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -66,5 +66,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i< height; i++)
+    {
+        for (int j = 0; j < width/2.0; j++)
+        {
+            
+            copy[i][j] = image[i][j]
+
+        }
+    }
     return;
 }
