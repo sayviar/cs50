099876fbe35d4f383406d5404798592336c30b5b
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-21 11:06:04 +0000

    Tue, May 21, 2024, 1:06 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,13 +5,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    //loop over all pixels
-    for (int i = 0; i< height; i++)
+    // loop over all pixels
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             int average;
-            average = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0);
+            average =
+                round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0);
             image[i][j].rgbtRed = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtBlue = average;
@@ -24,7 +25,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i< height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
@@ -38,12 +39,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
             (sepiaRed > 255) ? (image[i][j].rgbtRed = 255) : (image[i][j].rgbtRed = sepiaRed);
             sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
-            (sepiaGreen > 255) ? (image[i][j].rgbtGreen = 255) : (image[i][j].rgbtGreen = sepiaGreen);
+            (sepiaGreen > 255) ? (image[i][j].rgbtGreen = 255)
+                               : (image[i][j].rgbtGreen = sepiaGreen);
             sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
             (sepiaBlue > 255) ? (image[i][j].rgbtBlue = 255) : (image[i][j].rgbtBlue = sepiaBlue);
         }
     }
-
 
     return;
 }
@@ -51,13 +52,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-        for (int i = 0; i< height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width/2.0; j++)
+        for (int j = 0; j < width / 2.0; j++)
         {
             RGBTRIPLE buffer = image[i][j];
-            image[i][j] = image[i][width-1-j];
-            image[i][width-1-j] = buffer;
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = buffer;
         }
     }
     return;
@@ -67,46 +68,36 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copy[height][width];
-    for (int i = 0; i< height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
 
             copy[i][j] = image[i][j];
-
         }
     }
-    for (int i = 0; i< height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            //if (i != 0 && j !=0)
-            if (i == 0 && j == 0)
-            {
-            image[i][j].rgbtRed = round((copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed) / 4.0);
-            image[i][j].rgbtGreen = round((copy[i][j].rgbtGreen + copy[i][j+1].rgbtGreen + copy[i+1][j].rgbtGreen + copy[i+1][j+1].rgbtGreen) / 4.0);
-            image[i][j].rgbtBlue = round((copy[i][j].rgbtBlue + copy[i][j+1].rgbtBlue + copy[i+1][j].rgbtBlue + copy[i+1][j+1].rgbtBlue) / 4.0);
-            }
-            else if (i == 0)
-            {
-            image[i][j].rgbtRed = round((copy[i][j-1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j-1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed) / 6.0);
-            image[i][j].rgbtGreen = round((copy[i][j-1].rgbtGreen + copy[i][j].rgbtGreen + copy[i][j+1].rgbtGreen + copy[i+1][j-1].rgbtGreen + copy[i+1][j].rgbtGreen + copy[i+1][j+1].rgbtGreen) / 6.0);
-            image[i][j].rgbtBlue = round((copy[i][j-1].rgbtBlue + copy[i][j].rgbtBlue + copy[i][j+1].rgbtBlue + copy[i+1][j-1].rgbtBlue + copy[i+1][j].rgbtBlue + copy[i+1][j+1].rgbtBlue) / 6.0);
-            }
-            else if (j == 0 )
-            {
-            image[i][j].rgbtRed = round((copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed) / 6.0);
-            image[i][j].rgbtGreen = round((copy[i-1][j].rgbtGreen + copy[i-1][j+1].rgbtGreen + copy[i][j].rgbtGreen + copy[i][j+1].rgbtGreen + copy[i+1][j].rgbtGreen + copy[i+1][j+1].rgbtGreen) / 6.0);
-            image[i][j].rgbtBlue = round((copy[i-1][j].rgbtBlue + copy[i-1][j+1].rgbtBlue + copy[i][j].rgbtBlue + copy[i][j+1].rgbtBlue + copy[i+1][j].rgbtBlue + copy[i+1][j+1].rgbtBlue) / 6.0);
-            }
+            float red = 0, green = 0, blue = 0, count = 0;
 
-            else
+            for (int h = i - 1; h < i + 2; h++)
             {
-            image[i][j].rgbtRed = round((copy[i-1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j-1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j-1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed) / 9.0);
-            image[i][j].rgbtGreen = round((copy[i-1][j-1].rgbtGreen + copy[i-1][j].rgbtGreen + copy[i-1][j+1].rgbtGreen + copy[i][j-1].rgbtGreen + copy[i][j].rgbtGreen + copy[i][j+1].rgbtGreen + copy[i+1][j-1].rgbtGreen + copy[i+1][j].rgbtGreen + copy[i+1][j+1].rgbtGreen) / 9.0);
-            image[i][j].rgbtBlue = round((copy[i-1][j-1].rgbtBlue + copy[i-1][j].rgbtBlue + copy[i-1][j+1].rgbtBlue + copy[i][j-1].rgbtBlue + copy[i][j].rgbtBlue + copy[i][j+1].rgbtBlue + copy[i+1][j-1].rgbtBlue + copy[i+1][j].rgbtBlue + copy[i+1][j+1].rgbtBlue) / 9.0);
+                for (int w = j - 1; w < j + 2; w++)
+                {
+                    if ((h >= 0) && (h < height) && (w >= 0) && (w < width))
+                    {
+                        red += copy[h][w].rgbtRed;
+                        green += copy[h][w].rgbtGreen;
+                        blue += copy[h][w].rgbtBlue;
+                        count++;
+                    }
+                    image[i][j].rgbtRed = round(red / count);
+                    image[i][j].rgbtGreen = round(green / count);
+                    image[i][j].rgbtBlue = round(blue / count);
+                }
             }
-
         }
     }
 
---
+++ recover/recover.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("The usage is ./recover infile\n");
+        return 1;
+    }
+    // Open the memory card
+    char *infile = argv[1];
+    FILE *recover = fopen(infile, "r");
+    if (recover == NULL)
+    {
+        printf("File couldn't be opened\n");
+        return 1;
+    }
+    FILE *img = NULL;
+    char *filename = malloc(4);
+    int counter = 0;
+    uint8_t buffer[512];
+    // While there's still data left to read from the memory card
+    while (fread(buffer,1,512, recover) == 512)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+
+            if(img != NULL)
+            {
+                fclose(img);
+                counter++;
+
+            }
+            sprintf(filename, "%03i.jpg", counter);
+            img = fopen(filename,"w");
+            if (img == NULL)
+            {
+                return 1;
+            }
+
+
+
+        }
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, 512, img);
+        }
+    }
+        // Create JPEGs from the data
+    fclose(recover);
+    free(filename);
+}
