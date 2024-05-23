3ecb4438c0b4f4a0fc29aae66fa8fb4b39b022ac
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-23 12:10:50 +0000

    Thu, May 23, 2024, 2:10 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,8 +52,8 @@ person *create_family(int generations)
         new->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new->alleles[0] = parent0->alleles[rand()%2];
-        new->alleles[1] = parent1->alleles[rand()%2];
+        new->alleles[0] = parent0->alleles[rand() % 2];
+        new->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
@@ -80,14 +80,14 @@ void free_family(person *p)
     {
         return;
     }
-    else{
-    // TODO: Free parents recursively
-    free_family(p->parents[0]);
-    free_family(p->parents[1]);
+    else
+    {
+        // TODO: Free parents recursively
+        free_family(p->parents[0]);
+        free_family(p->parents[1]);
     }
     // TODO: Free child
     free(p);
-
 }
 
 // Print each family member and their alleles.
@@ -108,11 +108,13 @@ void print_family(person *p, int generation)
     // Print person
     if (generation == 0)
     {
-        printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0],
+               p->alleles[1]);
     }
     else if (generation == 1)
     {
-        printf("Parent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Parent (Generation %i): blood type %c%c\n", generation, p->alleles[0],
+               p->alleles[1]);
     }
     else
     {
@@ -120,7 +122,8 @@ void print_family(person *p, int generation)
         {
             printf("Great-");
         }
-        printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0],
+               p->alleles[1]);
     }
 
     // Print parents of current generation
