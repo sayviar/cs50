0e4994ea01779f9d810816f38fa62c73d182ff21
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-24 09:38:26 +0000

    Fri, May 24, 2024, 11:38 AM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -46,7 +46,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A' + toupper(word[1]) - 'A';
+    return toupper(word[0]) - 'A'+ toupper(word[1]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
