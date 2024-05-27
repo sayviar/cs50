61ae7004064c22d05d6cc80be5acdd2ce636e89a
sayviar <136386001+sayviar@users.noreply.github.com>
2024-05-27 13:03:05 +0000

    Mon, May 27, 2024, 3:03 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,4 +2,8 @@ from cs50 import get_int
 
 while True:
     size = get_int("Size of Pyramid (Size must be between 1 and 8): ")
-    if size >= 1 and size <= 8
+    if size >= 1 and size <= 8:
+        break
+
+for i in range(size):
+    for j in range(size):
