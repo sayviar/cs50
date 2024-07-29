bc1a0b2e229cd5da1f25c20e7135a4f926d895d2
sayviar <136386001+sayviar@users.noreply.github.com>
2024-07-29 15:41:51 +0000

    Mon, Jul 29, 2024, 5:41 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,10 +1,19 @@
 from cs50 import get_string
-letter = 0
-word = 0
-sentence = 0
+
 def main():
     text = get_string("Text: ")
-    counters(text)
+    letter = 0
+    word = 0
+    sentence = 0
+    for i in text:
+        if i.isalpha():
+            letter += 1
+        elif i.isspace():
+            word += 1
+        elif i in ['.', '!', '?']:
+            sentence += 1
+
+
     l = letter  / word * 100
     s = word  / sentence * 100
     liau = 0.0588 * l - 0.296 * s - 15.8
@@ -13,14 +22,4 @@ def main():
     else:
         print("Grade 16+")
 
-def counters(text):
-    for i in text:
-        if i.isalpha():
-            global letter += 1
-        elif i.isspace():
-            global word += 1
-        elif i in ['.', '!', '?']:
-            global sentence += 1
-
-    return True
 main()
