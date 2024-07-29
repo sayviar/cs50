fc0c155c4f7e67c66a631aa0d6d701d9fe8eb2ec
sayviar <136386001+sayviar@users.noreply.github.com>
2024-07-29 15:30:53 +0000

    Mon, Jul 29, 2024, 5:30 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,8 +5,8 @@ from cs50 import get_string
 def main():
     text = get_string("Text: ")
     counters(text)
-    l = letter / word * 100
-    s = word / sentence 
+    l = letter  / word
+    s = word  / sentence
 
 def counters(text):
     for i in text:
