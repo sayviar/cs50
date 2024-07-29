35ea8fb195310fb5e5e6cd69549b1c4ac5c094fd
sayviar <136386001+sayviar@users.noreply.github.com>
2024-07-29 15:09:07 +0000

    Mon, Jul 29, 2024, 5:09 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,14 @@
 from cs50 import get_string
-
+    letter = 0
+    word = 0
+    sentence = 0
 def main():
     text = get_string("Text: ")
-    word
+
+def counters(text):
     for i in text:
-        if i.isalpha()
+        if i.isalpha():
+            letter += 1
+        elif i 
+
+
