87d4db7c52845ef3a10ab40b7e25bd9e63cecfb9
sayviar <136386001+sayviar@users.noreply.github.com>
2024-07-29 10:57:00 +0000

    Mon, Jul 29, 2024, 12:57 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,12 @@
 from cs50 import get_string
+import re
 
 def main():
     while True:
         card_number = get_string
         if card_number.isnumeric():
             break
+    if re.search(")
 
-    match len
+
+
