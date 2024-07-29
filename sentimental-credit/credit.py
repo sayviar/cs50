c1ae901ada3ab6d3ddeac72367f21bdfcb7e9cda
sayviar <136386001+sayviar@users.noreply.github.com>
2024-07-29 12:54:30 +0000

    Mon, Jul 29, 2024, 2:54 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -34,7 +34,7 @@ def check_credit(credit_number):
     for i in checkstring:
         checksum += int(credit_number[i])
     for i in range(length, -1, -2):
-        checksum += int(credit_number[i])
+        checksum += int(checkstring[i])
 
     if checksum % 10 == 0:
         return True
