9f3c01351c5435e3290d266ef259440643f0c988
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 13:41:05 +0000

    Tue, Aug 6, 2024, 3:41 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -14,6 +14,7 @@ SELECT * FROM interviews WHERE year = 2023 AND month = 7 AND day = 28 AND transc
 
 -- Information that I note down to continue: - Thief left parking lot, looking into security footage of bakery to find his car.
 --                                           - Thief called someone less than a minute to take the earliest flight out of Fiftyville tomorrow
---                                           - Thief thief withdrew money 
+--                                           - Thief thief withdrew money early morning ATM on Legget Street
+
 
 
