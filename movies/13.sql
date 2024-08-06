88caf1e4f46f0614e4b74e17e0a0718298d7e5f0
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 11:46:07 +0000

    Tue, Aug 6, 2024, 1:46 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,4 @@
-SELECT name FROM people 
+SELECT name FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+GROUP BY movies.title HAVING people.name = 'Kevin Bacon';
