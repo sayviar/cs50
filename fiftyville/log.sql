b6f1005230a9239e1ab1d5dd44aca4b5e8b35b60
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 12:21:26 +0000

    Tue, Aug 6, 2024, 2:21 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- I search for the information in the chrim scene report to see what information are in general in there
+SELECT * FROM crime_scene_reports LIMIT 5;
