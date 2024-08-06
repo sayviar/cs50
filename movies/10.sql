8cdddd9f4f991d30d5f54a7342f07a17b984a3aa
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 12:07:44 +0000

    Tue, Aug 6, 2024, 2:07 PM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,4 @@
-SELECT UNIQUE name FROM people WHERE id IN
+SELECT DISTINCT name FROM people WHERE id IN
 (SELECT person_id FROM directors WHERE movie_id IN
-(SELECT movie_id FROM ratings WHERE rating > 9.0))
+(SELECT movie_id FROM ratings WHERE rating > 9.0));
 
