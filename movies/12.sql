0ffcfc98cb246e3fa8187ee31fa1078690d67750
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 11:39:35 +0000

    Tue, Aug 6, 2024, 1:39 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,5 @@
 SELECT title FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people on people.id = stars.person_id
-WHERE people.name = 'Jennier Lawrence' OR people.name = 'Bradley Cooper';
-GROUP BY title HAVING people.name = 'Jennier Lawrence' AND people.name = 'Bradley Cooper';
+WHERE people.name = 'Jennifer Lawrence' OR people.name = 'Bradley Cooper'
+GROUP BY title HAVING COUNT(name) = 2;
