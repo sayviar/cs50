12186e99467c244663994db95edbad49ec9deb8e
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 11:35:18 +0000

    Tue, Aug 6, 2024, 1:35 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
 SELECT title FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people on people.id = stars.person_id
-WHERE people.name = 'Bradley Cooper' AND people.name = 'Jennifer Lawrence';
+GROUP BY title HAVING people.name = 'Jennier Lawrence' AND people.name = 'Bradley Cooper';
