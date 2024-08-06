da74f86fd793a30f796724b2c830af0c4655845e
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 12:32:41 +0000

    Tue, Aug 6, 2024, 2:32 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,10 +1,15 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- I search for the information in the chrim scene report to see what information are in general in there
+-- I search for the information in the chrim scene report to see what information are in general in there.
 SELECT * FROM crime_scene_reports LIMIT 5;
 
--- I search in the same table for the specific street name 'Humphrey Street' and date 'July 28, 2023'  to see what descriptions it holds
+-- I search in the same table for the specific street name 'Humphrey Street' and date 'July 28, 2023'  to see what descriptions it holds.
 SELECT description FROM crime_scene_reports WHERE street = 'Humphrey Street' AND day = 28 AND month = 7 AND year = 2023;
+
+-- I search for all the interviews that have taken place on the same day as the theft.
+SELECT * FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
+
+-- There is a lot to take in so I decide to further filter it with the word theft in the transcript.
 
 
 
