f600093a4420920214c832cd9180821a5f87171b
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 12:20:49 +0000

    Tue, Aug 6, 2024, 2:20 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
---
+-- I search for the information in the chrim scene report to see what information are in general in there
