17e4ccc1cf87bd24e404f2a44fd22d1431e5a04f
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-06 14:28:17 +0000

    Tue, Aug 6, 2024, 4:28 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -41,13 +41,13 @@ SELECT * FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2023 AND durat
 
 -- Adjusting the earlier SQL Query to have more information to filter with for example airports and phone numbers
 
-SELECT people.name FROM passengers
+SELECT people.name,  FROM passengers
 JOIN flights ON flights.id = passengers.flight_id
 JOIN airports ON flights.origin_airport_id = airports.id
 JOIN people ON passengers.passport_number = people.passport_number
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
 JOIN phone_calls ON phone_calls.caller = people.phone_number
-WHERE atm_transactions.day = 29 AND atm_transactions.month = 7 AND atm_transactions.year = 2023 AND atm_transactions.atm_location = 'Leggett Street' AND atm_transactions.transaction_type = 'withdraw'
+WHERE flights.day = 29 AND flights.month = 7 AND flights.year = 2023 AND atm_transactions.atm_location = 'Leggett Street' AND atm_transactions.transaction_type = 'withdraw'
 AND city = 'Fiftyville' AND phone_calls.duration < 60;
 
