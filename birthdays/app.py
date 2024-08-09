8bf1d1035ff1a70f2ba88795df37738e317a1bc2
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-09 12:50:07 +0000

    Fri, Aug 9, 2024, 2:50 PM +02:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -40,6 +40,7 @@ def index():
         birthdays = db.execute("SELECT * FROM birthdays")
 
         return render_template("index.html", birthdays=birthdays)
+    
 @app.route("/delete", methods=["POST"])
 def delete():
     if request.method == "POST":
