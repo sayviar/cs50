af6b2f50a59186b07976a91797f134729d19c230
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-12 08:21:58 +0000

    Mon, Aug 12, 2024, 10:21 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -64,7 +64,7 @@ def buy():
             db.execute("INSERT INTO portfolio (user_id, symbol, shares) VALUES(?,?,?)", session["user_id"], symbol, shares)
             db.execute("INSERT INTO history (user_id, symbol, shares, share_price, total) VALUES(?,?,?,?,?)", session["user_id"], symbol, shares, quote["price"], quote["price"] * 100 * shares)
             return redirect("/")
-        if stock["shares"]
+        db.execite("UPDATE portfolio )
     return render_template("buy.html")
 
 
