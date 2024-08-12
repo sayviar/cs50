c5fcc60ad8e6edc5e4a48ce67fd095c7af837cc9
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-12 09:42:42 +0000

    Mon, Aug 12, 2024, 11:42 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -40,7 +40,11 @@ def index():
                            JOIN users ON users.id = port.user_id
                            JOIN history hist ON hist.user_id = port.user_id AND hist.symbol = port.symbol
                            WHERE users.id = ?""", session["user_id"])
-    return render_template("portfolio.html", portfolio = portfolio)
+    cash = portfolio[0]["users.cash"]
+    for row in portfolio:
+        cash -= row["total"]
+    total = portfolio[0]["users.cash"]
+    return render_template("portfolio.html", portfolio = portfolio, cash = cash, total = total)
 
 
 
