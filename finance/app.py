599a2a0729843b12f6dd08829731e04998304e23
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-13 08:08:58 +0000

    Tue, Aug 13, 2024, 10:08 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -72,7 +72,7 @@ def buy():
          #   "SELECT SUM(round(shares * share_price,2)) as spent FROM history WHERE user_id = ?", session["user_id"])[0]["spent"]
         #if spent == None:
          #   spent = 0
-        #money = money[0]["cash"] - spent
+        money = money[0]["cash"]
         if money * 100 < quote["price"] * 100 * shares:
             return apology("Sorry, your balance isn't enough for this purchase.")
 
