3463846a33eaefd7469c6edfdb5f75b6458c9821
sayviar <136386001+sayviar@users.noreply.github.com>
2024-08-13 08:11:51 +0000

    Tue, Aug 13, 2024, 10:11 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -86,7 +86,7 @@ def buy():
                        shares, session["user_id"], symbol)
         db.execute("INSERT INTO history (user_id, symbol, shares, share_price, total) VALUES(?,?,?,?,?)",
                    session["user_id"], symbol, shares, quote["price"], quote["price"] * 100 * (-shares))
-        db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", quote["price"] * (-shares), session["user_id"])
+        db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", quote["price"] * (shares), session["user_id"])
 
         return redirect("/")
 
